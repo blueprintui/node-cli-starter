3ba1f513daebcb0999ca2ac77affeb525534f63b
Cory Rylan <cory@coryrylan.com>
2025-07-19 18:36:22 -0500

    chore: add basic cmd setup
    Signed-off-by: Cory Rylan <cory@coryrylan.com>

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,28 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-yargs(hideBin(process.argv))
-  .command('greet [message]', 'create a greeting', (yargs) => {
-    return yargs
-      .positional('message', {
-        describe: 'message to greet'
-      })
-  }, (argv) => {
-    const greeting = argv.message || 'world';
-    const capitalized = argv.capitalize ? greeting.toUpperCase() : greeting;
-    console.log(`hello, ${capitalized}!`);
-  })
-  .option('capitalize', {
-    alias: 'c',
-    type: 'boolean',
-    description: 'Capitalize the message'
-  })
-  .parse()
+const cli = yargs(hideBin(process.argv))
+  .scriptName('ncs')
+  .version('0.0.0')
+  .usage('$0 <cmd> [args]');
+
+cli.command('$0', 'show help', () => { }, async () => {
+  console.log(await cli.getHelp());
+});
+
+cli.command('greet [message]', 'create a greeting', (yargs) => {
+  return yargs
+    .positional('message', {
+      describe: 'message to greet'
+    })
+}, (argv) => {
+  const greeting = argv.message || 'world';
+  const capitalized = argv.capitalize ? greeting.toUpperCase() : greeting;
+  console.log(`hello, ${capitalized}!`);
+})
+    .option('capitalize', {
+      alias: 'c',
+      type: 'boolean',
+      description: 'Capitalize the message'
+    })
+    .parse()
